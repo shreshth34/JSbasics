1bf03143bd8b2fca165048a644af77e5a8449632
shreshth <shreshth.j@talview.com>
2020-01-20 10:46:16 +0530

    practice question in ques.js

---
+++ ques.js
@@ -0,0 +1,6 @@
+//reverse string in js
+
+var str = "i love javascript";
+console.log(str.split('').reverse().join(''));
+
+
--- regExp.js
+++ regExp.js
@@ -1,21 +1,16 @@
 //two ways to create regular exprssion
 let re1 = new RegExp("abc");
-
 let re2 = /abc/; //every forward slash will have a back slash in regex
-
-
 
 //for special character like +,-,? etc use backlash'/'
 let eighteenPlus = /eighteen\+/ ;
 
 
-
 //Test method for regex gives a boolean telling about the string match or not.
 console.log(/abc/.test("ababdc"));
 //false
-console.log(/abde/.test("xacabde"));
+console.log(/'abde/.test("xacabde"));
 //true
-
 
 
 //string contains digit or not
@@ -25,11 +20,10 @@ console.log(/[0-6]/.test("in 9999"));
 //false
 
 
-
 //to check a validity of date or any digit us '\d'
 let birthDate = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
 console.log(birthDate.test("26-03-1998 12:00"));
 //true
 
-
+console.log(/'\d+'/.test("'1231'"));
 
