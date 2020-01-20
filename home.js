8ac75f87fb10a07234234f05b34d077cc4a2eed9
shreshth <shreshth.j@talview.com>
2020-01-20 09:18:18 +0530

    "4 topics of JS  at start"

---
+++ home.js
@@ -0,0 +1,10 @@
+console.log("Basics of JS");
+
+var JS1 = {
+
+    topic1: "Values, numbers and operators",
+    topic2: "Program Structure",
+    topic3: "Functions",
+    topic4: "Higher order functions"
+
+}
