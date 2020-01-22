82f5fa0ca44eb05bcc247f5b0afd05fc8ad05f66
Shreshth  Juneja <INTERNAL\shreshth.j@shreshth.internal.talview.com>
2020-01-22 12:45:21 +0530

    12:45 edit

--- functions.js
+++ functions.js
@@ -1,146 +1,164 @@
-//Higher order functions
-repeat = (n, action) => {
+// //Higher order functions
+// repeat = (n, action) => {
 
-    for (let i=0; i<n ; i++){
-        action(i);
-    }
-}
-repeat(3, console.log);
+//     for (let i=0; i<n ; i++){
+//         action(i);
+//     }
+// }
+// repeat(3, console.log);
 
-let labels = [];
-repeat(5, (i) => {
+// let labels = [];
+// repeat(5, (i) => {
 
-    labels.push(`Unit ${i+1}`);
+//     labels.push(`Unit ${i+1}`);
 
-});
-console.log(labels);
+// });
+// console.log(labels);
 
-greaterThan = (n) =>{
+// greaterThan = (n) =>{
 
-    return m => m>n;
+//     return m => m>n;
 
-}
-let greaterthan10 = greaterThan(10);
-console.log(greaterthan10(11));
+// }
+// let greaterthan10 = greaterThan(10);
+// console.log(greaterthan10(11));
   
-const companies = [
+// const companies = [
 
-{name: "one", category: "finance", start: "1998"},
-{name: "two", category: "design", start: "1911"},
-{name: "three", category: "SEO", start: "1908"},
-{name: "four", category: "marketing", start: "1928"},
-{name: "five", category: "finance", start: "1930"},
-{name: "six", category: "legal", start: "1940"},
-{name: "seven", category: "media", start: "1994"}
+// {name: "one", category: "finance", start: "1998"},
+// {name: "two", category: "design", start: "1911"},
+// {name: "three", category: "SEO", start: "1908"},
+// {name: "four", category: "marketing", start: "1928"},
+// {name: "five", category: "finance", start: "1930"},
+// {name: "six", category: "legal", start: "1940"},
+// {name: "seven", category: "media", start: "1994"}
 
-];
+// ];
 
-const ages = [22,34,24,55,64,36,54,45,97,33,29,5,45,53,67,89,25]; 
+// const ages = [22,34,24,55,64,36,54,45,97,33,29,5,45,53,67,89,25]; 
 
-companies.forEach(function(e){
+// companies.forEach(function(e){
 
-    console.log(e.category);
+//     console.log(e.category);
 
-});
+// });
 
-//attempt 1
-ages.forEach(function(e){
+// //attempt 1
+// ages.forEach(function(e){
 
-    if(e>22){
-        console.log(e);
-    }
+//     if(e>22){
+//         console.log(e);
+//     }
 
-});
+// });
 
-//alternative for attempt 1
-const canDrink = ages.filter(e => e>=22);
-console.log(canDrink);
+// //alternative for attempt 1
+// const canDrink = ages.filter(e => e>=22);
+// console.log(canDrink);
 
-const finance1 = companies.filter(e => e.category == "finance");
-console.log(finance1);
+// const finance1 = companies.filter(e => e.category == "finance");
+// console.log(finance1);
 
-printSome = (n) => {
-return m => m+" "+n;
-}
+// printSome = (n) => {
+// return m => m+" "+n;
+// }
 
-const jump = printSome("cool");
-console.log(jump("something"));
+// const jump = printSome("cool");
+// console.log(jump("something"));
 
-//create array of company names
+// //create array of company names
 
-const ageSquare = ages.map(age => Math.sqrt(age));
-console.log(ageSquare);
+// const ageSquare = ages.map(age => Math.sqrt(age));
+// console.log(ageSquare);
 
-//const ages = [22,34,24,55,64,36,54,45,97,33,29,5,45,53,67,89,25]; 
+// //const ages = [22,34,24,55,64,36,54,45,97,33,29,5,45,53,67,89,25]; 
 
-const ageSort = ages.sort((a,b)=> a<b ? -1 : 1);
-console.log(ageSort);
+// const ageSort = ages.sort((a,b)=> a<b ? -1 : 1);
+// console.log(ageSort);
 
-const addAge = ages.reduce((total,e) => total+e,0) ;
-console.log(addAge);
+// const addAge = ages.reduce((total,e) => total+e,0) ;
+// console.log(addAge);
 
-list1 = [1,2,2,3,4,2,3,4,6,7,8,8,8,7,4,5];
+// list1 = [1,2,2,3,4,2,3,4,6,7,8,8,8,7,4,5];
 
-console.log([...new Set(list1)]);
+// console.log([...new Set(list1)]);
 
-//from Eloquent Javascript -- | https://eloquentjavascript.net/03_functions.html
+// //from Eloquent Javascript -- | https://eloquentjavascript.net/03_functions.html
 
-//square function
+// //square function
 
-const square = function(x){
+// const square = function(x){
 
-    return x*x;
+//     return x*x;
 
-};
+// };
 
 
-/*Consider this puzzle: by starting from 
-the number 1 and repeatedly either adding 5 or 
-multiplying by 3, an infinite set of numbers can be 
-produced. How would you write a function that, given 
-a number, tries to find a sequence 
-additions and multiplications that produces that number?*/
+// /*Consider this puzzle: by starting from 
+// the number 1 and repeatedly either adding 5 or 
+// multiplying by 3, an infinite set of numbers can be 
+// produced. How would you write a function that, given 
+// a number, tries to find a sequence 
+// additions and multiplications that produces that number?*/
 
-function findSolution(target) {
-    function find(current, history) {
-      if (current == target) {
-        return history;
-      } else if (current > target) {
-        return null;
-      } else {
-        return find(current + 5, `(${history} + 5)`) ||
-               find(current * 3, `(${history} * 3)`);
-      }
-    }
-    return find(1, "1");
-  }
+// function findSolution(target) {
+//     function find(current, history) {
+//       if (current == target) {
+//         return history;
+//       } else if (current > target) {
+//         return null;
+//       } else {
+//         return find(current + 5, `(${history} + 5)`) ||
+//                find(current * 3, `(${history} * 3)`);
+//       }
+//     }
+//     return find(1, "1");
+//   }
   
-  console.log(findSolution(21));
+//   console.log(findSolution(21));
 
 
-//printFarmInventory(7,11);
-//007 Cows
-//011 Chickens
+// //printFarmInventory(7,11);
+// //007 Cows
+// //011 Chickens
 
 
-function printZeroPaddedWithLabel(number, label) {
-    let numberString = String(number);
-    while (numberString.length < 3) {
-      numberString = "0" + numberString;
-    }
-    console.log(`${numberString} ${label}`);
-  }
+// function printZeroPaddedWithLabel(number, label) {
+//     let numberString = String(number);
+//     while (numberString.length < 3) {
+//       numberString = "0" + numberString;
+//     }
+//     console.log(`${numberString} ${label}`);
+//   }
                                                
   
-  function printFarmInventory(cows, chickens, pigs) {
-    printZeroPaddedWithLabel(cows, "Cows");
-    printZeroPaddedWithLabel(chickens, "Chickens");
-    printZeroPaddedWithLabel(pigs, "Pigs");
-  }
+//   function printFarmInventory(cows, chickens, pigs) {
+//     printZeroPaddedWithLabel(cows, "Cows");
+//     printZeroPaddedWithLabel(chickens, "Chickens");
+//     printZeroPaddedWithLabel(pigs, "Pigs");
+//   }
   
-printFarmInventory(7, 11, 3);
+// printFarmInventory(7, 11, 3);
 
 
+//using a contructor for function
+let great = Function("n", "return 2020-n;");
+console.log(great(1998));
+
+// //get date
+// const ordinal = require("ordinal");
+// const {days, months} = require("date-names");
+
+// exports.formatDate = function(date, format) {
+//   return format.replace(/YYYY|M(MMM)?|Do?|dddd/g, tag => {
+//     if (tag == "YYYY") return date.getFullYear();
+//     if (tag == "M") return date.getMonth();
+//     if (tag == "MMMM") return months[date.getMonth()];
+//     if (tag == "D") return date.getDate();
+//     if (tag == "Do") return ordinal(date.getDate());
+//     if (tag == "dddd") return days[date.getDay()];
+//   });
+// };
 
 
 
--- lodash/index.js
+++ lodash/index.js
@@ -1,17 +1,34 @@
 const _ = require("lodash");
 
-const name = "shreshth"; 
+// const name = "shreshth"; 
 
-if (_.isString(name)){
+// if (_.isString(name)){
 
-    console.log("working");
+//     console.log("working");
 
-}
-else{
+// }
+// else{
 
-    console.log("not working");
+//     console.log("not working");
 
-}
+// }
 
-console.log(_.upperCase(name)); 
-console.log(_.random(0,9));
+// console.log(_.upperCase(name)); //SHREsHTH
+// console.log(_.random(0,9)); //random number
+
+// //create chunks of array
+
+// const x = _.chunk([1,2,3,4,5,6],2);
+// console.log(x);
+
+// console.log(_.compact([0,1,false,2,'',3]));  //_.compact removes falsey values
+
+
+
+
+//concat function
+var arr1 = [2];
+var other = _.concat(arr1, 1, 2, [3], [[4]], "new");
+console.log("first array", arr1);
+console.log("concat array", other);
+
